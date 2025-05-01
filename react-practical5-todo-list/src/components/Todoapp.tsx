0fa53336635749a9fd89dfbbe04b5a182a2d9868
Hetal Desai <hetal.desai@simformsolutions.com>
2025-05-01 17:16:32 +0530

    Add: Add TODO functionality

--- react-practical5-todo-list/src/components/Todoapp.tsx
+++ react-practical5-todo-list/src/components/Todoapp.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TodoApp.css";
 import { FaEdit, FaTrashAlt, FaCheckCircle } from "react-icons/fa";
+import { Todo } from "./Types";
 
 const TodoAppUI: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [input, setInput] = useState("");
+
+  const handleAdd = () => {
+    if (input.trim() === "") return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: input.trim(),
+      completed: false,
+      date: new Date().toLocaleString(),
+    };
+
+    setTodos([newTodo, ...todos]);
+
+    setInput("");
+  };
+
   return (
     <div className="todo-container">
       <div className="todo-header">
-        <input type="text" placeholder="Enter Todo" />
-        <button>Add Todo</button>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleAdd()}
+          placeholder="Enter Todo"
+        />
+        <button onClick={handleAdd}>Add Todo</button>
       </div>
 
       <div className="filter">
